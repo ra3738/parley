b4c0978e57bfe21a0bb79ced5af96715b7c51837
isthisevenreal <rithvik.al@gmail.com>
2020-08-16 02:12:54 -0700

    ChatScreen

--- App.js
+++ App.js
@@ -8,47 +8,18 @@
 
 import React from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
+  FlatList,
   ScrollView,
   View,
   Text,
+  Image,
   Button,
-  StatusBar,
+  TouchableOpacity,
 } from 'react-native';
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
-const HomeScreen = ({navigation}) => {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
-      />
-    </View>
-  );
-}
-
-const DetailsScreen = ({navigation}) => {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-      <Button
-        title="Go to Details... again"
-        onPress={() => navigation.push('Details')}
-      />
-    </View>
-  );
-}
+import ChatScreen from './ChatScreen'
 
 const Stack = createStackNavigator();
 
@@ -56,8 +27,7 @@ const App = ({navigation}) => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="Chats" component={ChatScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -65,7 +35,7 @@ const App = ({navigation}) => {
 
 const styles = StyleSheet.create({
   body: {
-    backgroundColor: Colors.white,
+    flex: 1
   },
 });
 
---
+++ ChatScreen.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import {
+  StyleSheet,
+  FlatList,
+  ScrollView,
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+
+const ChatScreen = ({navigation}) => {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center' }}>
+        <FlatList
+          data={[
+            {key: 'Devin'},
+            {key: 'Dan'},
+            {key: 'Dominic'},
+            {key: 'Jackson'},
+            {key: 'James'},
+          ]}
+          renderItem={({item}) => 
+            <TouchableOpacity onPress={() => navigation.navigate('Chats')} style={styles.appButtonContainer}>
+              <Image style={styles.profilepic} source={require('./lake.jpg')}/>
+              <View style={styles.texts}>
+                <Text style={styles.nameText}>{item.key}</Text>
+                <Text style={styles.lastMessage}>abc</Text>
+              </View>
+            </TouchableOpacity>
+          }
+        />
+      </View>
+    );
+  }
+
+  const styles = StyleSheet.create({
+    profilepic: {
+      width: 60,
+      height: 60,
+      borderRadius: 60 / 2,
+    },
+    texts: {
+      flexDirection: 'column',
+    },
+    appButtonContainer: {
+      borderBottomWidth: 1,
+      borderBottomColor: '#e6e4e1',
+      padding: 10,
+      flexDirection: 'row',
+      // alignItems: 'center',
+    },
+    nameText: {
+      paddingLeft: 20,
+      paddingTop: 5,
+      paddingBottom: 10,
+      fontSize: 18,
+      fontWeight: 'bold',
+    },
+    lastMessage: {
+      paddingLeft: 20,
+      fontSize: 14,
+    }
+  });
+
+  export default ChatScreen;
